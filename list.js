2e92409a4c1bd53675c7eb2123ffc30c35a99c38
Peter <github@pixeldepth.net>
2013-07-15 14:45:57 +0100

    Notification and testing of 1 trophy popup

---
+++ list.js
@@ -0,0 +1,19 @@
+trophies.list = (function(){
+
+	return {
+	
+		"logged_in": {
+
+			id: "1",
+			cup: "bronze",
+			title: "Logged In",
+			image: "logged_in",
+			method: "logged_in",
+			description: "You are logged in to your account",
+			disabled: false
+			
+		}
+	
+	};
+
+})();
--- trophies.js
+++ trophies.js
@@ -10,12 +10,23 @@ var trophies = {
 	route: null,
 	params: null,
 	images: null,
-		
+
+	sound_host: "http://pixeldepth.net/proboards/plugins/trophies/sound/",
+	image_host: "http://pixeldepth.net/proboards/plugins/trophies/images/",
+	
 	modules: [],
 	
+	cups: {
+
+		gold: "images/icons/cups/gold.png",
+		silver: "images/icons/cups/silver.png",
+		bronze: "images/icons/cups/bronze.png"
+
+	},
+	
 	init: function(){
-		console.log(1);
 		this.setup();
+		this.build_notification();
 	},
 		
 	setup: function(){
@@ -34,6 +45,32 @@ var trophies = {
 		if(this.plugin && this.plugin.settings){
 			var settings = this.plugin.settings;
 		}
+	},
+	
+	build_notification: function(){
+		var notification = "";
+		
+		notification += "<div id='trophy-notification' style='display: none;'>";
+		notification += "<div class='trophy-notification-left'><img id='trophy-notification-img' src='" + this.image_host + "images/misc/blank.png' /></div>";
+		notification += "<div id='trophy-notification-title' class='trophy-notification-left'>You have earned a trophy.";
+		notification += "<p id='trophy-notification-info'><img id='trophy-notification-cup' src='" + this.image_host + "images/misc/blank.png' /> ";
+		notification += "<span id='trophy-notification-txt'></span></p></div></div>";
+		
+		$("body").append($(notification));
+	},
+	
+	show_notification: function(trophy){
+		var notification = $("#trophy-notification");
+		
+		notification.find("#trophy-notification-img").attr("src", this.image_host + "trophies/" + trophy.image + ".png");
+		notification.find("#trophy-notification-cup").attr("src", this.image_host + "icons/cups/" + trophy.cup + ".png");
+		notification.find("#trophy-notification-txt").html(trophy.title);
+		
+		notification.fadeIn("normal", function(){
+			$(this).delay(5000).fadeOut("normal");
+		});
+		
+		yootil.sound.play(this.sound_host + "trophy.mp3");
 	}
 	
 };
